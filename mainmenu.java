93c44cb9fcd38f7ab529af20335be21badb49263
s3722279 <48265234+s3722279@users.noreply.github.com>
2019-05-25 18:07:40 +1000

    Add files via upload

--- mainmenu.java
+++ mainmenu.java
@@ -1,6 +1,7 @@
 package sefA1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class mainmenu{
@@ -86,10 +87,10 @@ public class mainmenu{
 				
 				Scanner sc = new Scanner(System.in);
 				int ID = sc.nextInt();
-				//
+				
 				for(int i = 0 ; i < array.size(); i++){
 					Product item = array.get(i);
-					//
+					
 					if(item.ID == ID){
 						System.out.println("Enter new item ID");
 						item.ID = sc.nextInt();
@@ -196,11 +197,171 @@ public class mainmenu{
 		}
 
 		private static void mainMenu() {
-			// TODO Auto-generated method stub
+			
+
+		}
+		private static void checkPrice() {
+		
+
 
 		}
 
 
+		private static void customerInput()
+		{
+			Scanner sc = new Scanner(System.in);
+			char custAnswer;
+			char finalAns;	
+			do {
+			System.out.println("What would you like to do? \nSelect Product: s		Check Price: p		Check Discounts for an item: d"); //Change the question??
+			custAnswer = sc.next().charAt(0);
+			}
+			while ((custAnswer != 's') && (custAnswer != 'p') && (custAnswer != 'd'));
+			
+			switch(custAnswer)
+			{
+			case 's':
+			s();
+			break; 
+			
+			case 'p':
+			p();
+			break;
+			
+			case 'd':
+			d();
+			break;
+					
+			default:
+				System.out.println("Error - please enter a valid option.")	
+			break;
+					
+			}
+			
 		}
+			public static String s()
+			{
+				Scanner sc = new Scanner(System.in);
+				char custAnswer;
+				char finalAnswer;	
+				while(true)
+				{
+					do {
+					//Show all the products
+					//Product array	
+					
+					//Products:					selection:
+					//Bread							a
+					//Milk							b
+					//Eggs							c
+					System.out.println("Products:		Selection:");
+					System.out.println("bread:				a");
+					System.out.println("milk:				b");
+					System.out.println("eggs				c");
+						
+						
+						System.out.println("Select product using the characters to the right of the product names:");
+						custAnswer = sc.next().charAt(0);
+						}
+
+					while ((custAnswer != 'a') && (custAnswer != 'b') && (custAnswer != 'c'));
+					
+						if (custAnswer == 'a')
+						{
+							 System.out.println("a"); //From the product class 
+							//Show all the details about the product bread, E.g price, supplier, discount when bought in bulk
+						}
+						else if (custAnswer == 'b')
+						{
+							System.out.println("b"); //From the product class
+							//Show all the details about the product milk, E.g price, supplier, discount when bought in bulk
+						}
+						else if (custAnswer == 'c')
+						{
+							System.out.println("c"); //From the product class
+							//Show all the details about the product eggs, E.g price, supplier, discount when bought in bulk
+						}
+						System.out.println("Select another product: s");
+						System.out.println("Exit the program: e");
+						finalAnswer = sc.next().charAt(0);
+						if(finalAnswer == 'e')
+						{
+							System.exit(0);
+						}
+						if (finalAnswer == 's') continue;
+			}
+			}
+			
+			public static String p()
+			{
+				while(true) 
+				{
+					
+				String[] values = {"p1000", "p1500", "p2000"};  //change this to the product array where the product's details is contained
+				Scanner sc = new Scanner(System.in);
+				String custAnswer;
+				char finalAnswer;
+				do {
+						System.out.println("Enter ProductID:");
+						custAnswer = sc.nextLine();}
+						//change the "values" into the product array with the productID so it can search for the productID and see if it matches
+						while(Arrays.stream(values).noneMatch(custAnswer::equals));
+						if(Arrays.stream(values).anyMatch(custAnswer::equals)) 
+						{
+							System.out.println("Product details are as follows:");
+							//Show all the details about the product bread, E.g price, supplier, discount when bought in bulk
+						}
+						
+						System.out.println("Enter another productID: p");
+						System.out.println("Exit the program: e");
+						finalAnswer = sc.next().charAt(0);
+						if(finalAnswer == 'e')
+						{
+							System.exit(0);
+						}
+						if (finalAnswer == 'p') continue;
+			}
+			
+		}
+			public static String d()
+			{
+				while(true) 
+				{
+					
+				String[] values = {"p1000", "p1500", "p2000"};  //change this to the product array where the product's details is contained
+				Scanner sc = new Scanner(System.in);
+				String custAnswer;
+				char finalAnswer;
+				do {
+						System.out.println("Enter ProductID:");
+						custAnswer = sc.nextLine();}
+						//change the "values" into the product array with the productID so it can search for the productID and see if it matches
+						while(Arrays.stream(values).noneMatch(custAnswer::equals));
+						if(Arrays.stream(values).anyMatch(custAnswer::equals)) 
+						{
+							System.out.println("Product details are as follows:");
+							//Show all the details about the product bread, E.g price, supplier, discount when bought in bulk
+						}
+						
+						System.out.println("Enter another productID: p");
+						System.out.println("Exit the program: e");
+						finalAnswer = sc.next().charAt(0);
+						if(finalAnswer == 'e')
+						{
+							System.exit(0);
+						}
+						if (finalAnswer == 'p') continue;
+			}
+			}
+
+}
+
+
+			
+			
+		
+	
+
+
 
 
